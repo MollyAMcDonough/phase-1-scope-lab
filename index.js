cfe8d89f72ea870eb7d175f295a90838dbf80ab8
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-02-02 11:49:57 -0500

    commit tree 75d1b44863969d5d4c041a7224e9e3986ca0cb17

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+/*
+const setBestCustomer = () => {
+    bestCustomer = 'not bob';
+};
+setBestCustomer();
+console.log("setBestCustomer: ",bestCustomer);
+
+const overwriteBestCustomer = (cust) => {
+    bestCustomer = cust;
+};
+
+overwriteBestCustomer('maybe bob');
+console.log("overWriteBestCustomer: ",bestCustomer);
+
+const changeLeastFavoriteCustomer = () => {
+    leastFavoriteCustomer = 'something else';
+};
+*/
+
+function setBestCustomer () {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer (cust) {
+    bestCustomer = cust;
+}
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'not the best'
+}
